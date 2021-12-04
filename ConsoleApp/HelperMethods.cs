1212447d14f63c428307312e8061b12e8ed17539
Owen <jenkinsojj@gmail.com>
2021-12-04 01:47:19 +0000

    Initial commit. Console App for Advent of Code.
    Days 1 + 2 complete. Started Day 3.

---
+++ ConsoleApp/HelperMethods.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace ConsoleApp
+{
+    public static class HelperMethods
+    {
+        public static int[] GetArrayOfNumbersFromFile(string filepath)
+        {
+            string[] lines = File.ReadAllLines(filepath);
+
+            List<int> numbersList = new List<int>();
+
+            int number;
+            for (int i = 0; i < lines.Length; i++)
+            {
+                bool succeeded = int.TryParse(lines[i], out number);
+
+                if (succeeded)
+                {
+                    numbersList.Add(number);
+                }
+            }
+
+            return numbersList.ToArray();
+        }
+    }
+}
---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,193 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine($"Solution to Day 1 Part 1 is {Day1SonarSweepPart1(@"./data/day01_input.txt")}");
+            //Console.WriteLine($"Solution to Day 1 Part 2 is {Day1SonarSweepPart2(@"./data/day01_input.txt")}");
+
+            //Console.WriteLine($"Solution to Day 2 Part 1 is {Day2DivePart1(@"./data/day02_input.txt")}");
+            //Console.WriteLine($"Solution to Day 2 Part 2 is {Day2DivePart2(@"./data/day02_input.txt")}");
+
+            Console.WriteLine($"Solution to Day 3 Part 1 is {Day3BinaryDiagnosticPart1(@"./data/day03_input.txt")}");
+
+            Console.ReadLine();
+        }
+
+        private static int Day1SonarSweepPart1(string filepath)
+        {
+            const int minNumberMeasurements = 2;
+
+            int[] sonarMeasurements = HelperMethods.GetArrayOfNumbersFromFile(filepath);
+
+            if (sonarMeasurements.Length < minNumberMeasurements)
+            {
+                return 0;
+            }
+
+            int increaseCount = 0;
+            for (int i = 1; i < sonarMeasurements.Length; i++)
+            {
+                if (sonarMeasurements[i] > sonarMeasurements[i-1])
+                {
+                    increaseCount++;
+                }
+            }
+            
+            return increaseCount;
+        }
+
+        private static int Day1SonarSweepPart2(string filepath, int slidingWindowLength = 3)
+        {
+            int minNumberMeasurements = slidingWindowLength + 1;
+
+            int[] sonarMeasurements = HelperMethods.GetArrayOfNumbersFromFile(filepath);
+
+            if (sonarMeasurements.Length < minNumberMeasurements)
+            {
+                return 0;
+            }
+
+            int increaseCount = 0;
+            int window1Sum = 0; // window 1 = upper window
+            int window2Sum = 0; // window 2 = lower window
+            int startIndex = minNumberMeasurements - 1;
+
+            // find sums in first window positions
+            for (int i = 0; i < slidingWindowLength; i++)
+            {
+                window1Sum += sonarMeasurements[i + 1];
+                window2Sum += sonarMeasurements[i];
+            }
+
+            for (int i = startIndex; i < sonarMeasurements.Length; i++)
+            {
+                int window1UpperIndex = i;
+                int window2UpperIndex = i - 1;
+
+                if (i > startIndex)
+                {
+                    // calculate the new sum. add the new value at the top of the window; remove the old value at the bottom.
+                    window1Sum = window1Sum - sonarMeasurements[window1UpperIndex - slidingWindowLength] + sonarMeasurements[window1UpperIndex];
+                    window2Sum = window2Sum - sonarMeasurements[window2UpperIndex - slidingWindowLength] + sonarMeasurements[window2UpperIndex];
+                }
+
+                if (window1Sum > window2Sum)
+                {
+                    increaseCount++;
+                }
+            }
+
+            return increaseCount;
+        }
+
+        private static int Day2DivePart1(string filepath)
+        {
+            string[] lines = File.ReadAllLines(filepath);
+            int horizontal = 0;
+            int depth = 0;
+
+            // assuming no errors/inconsistencies in the input data!
+            foreach (string line in lines)
+            {
+                string[] split = line.Split(' ');
+
+                string commandString = split[0];
+                int commandMagnitude = Convert.ToInt32(split[1]);
+
+                if (commandString == "forward")
+                {
+                    horizontal += commandMagnitude;
+                }
+                else if (commandString == "down")
+                {
+                    depth += commandMagnitude;
+                }
+                else if (commandString == "up")
+                {
+                    depth -= commandMagnitude;
+                }
+            }
+
+            return horizontal * depth;
+        }
+
+        private static int Day2DivePart2(string filepath)
+        {
+            string[] lines = File.ReadAllLines(filepath);
+            int horizontal = 0;
+            int depth = 0;
+            int aim = 0;
+
+            // assuming no errors/inconsistencies in the input data!
+            foreach (string line in lines)
+            {
+                string[] split = line.Split(' ');
+
+                string commandString = split[0];
+                int commandMagnitude = Convert.ToInt32(split[1]);
+
+                if (commandString == "forward")
+                {
+                    horizontal += commandMagnitude;
+                    depth += (aim * commandMagnitude);
+                }
+                else if (commandString == "down")
+                {
+                    aim += commandMagnitude;
+                }
+                else if (commandString == "up")
+                {
+                    aim -= commandMagnitude;
+                }
+            }
+
+            return horizontal * depth;
+        }
+
+        private static double Day3BinaryDiagnosticPart1(string filepath)
+        {
+            string[] lines = File.ReadAllLines(filepath);
+
+            int maxNumBits = 0;
+
+            foreach (string line in lines)
+            {
+                if (line.Length > maxNumBits)
+                {
+                    maxNumBits = line.Length;
+                }
+            }
+
+            int[] gammaRateBits = new int[maxNumBits];
+            int[] epsilonRateBits = new int[maxNumBits];
+            double gammaDecimalNum = 0;
+            double epsilonDecimalNum = 0;
+
+            foreach (string line in lines)
+            {
+                for (int i = 0; i < line.Length; i++)
+                {
+                    gammaRateBits[i] += Convert.ToInt32(line[i].ToString());
+                }
+            }
+
+            for (int i = 0; i < maxNumBits; i++)
+            {
+                gammaRateBits[i] = (gammaRateBits[i] > lines.Length - gammaRateBits[i]) ? 1 : 0;
+                epsilonRateBits[i] = (gammaRateBits[i] == 1) ? 0 : 1;
+
+                int exponent = maxNumBits - i - 1;
+                gammaDecimalNum += gammaRateBits[i] * Math.Pow(2, exponent);
+                epsilonDecimalNum += epsilonRateBits[i] * Math.Pow(2, exponent);
+            }
+
+            return gammaDecimalNum * epsilonDecimalNum;
+        }
+    }
+}
